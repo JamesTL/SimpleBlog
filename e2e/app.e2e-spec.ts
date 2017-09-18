34e7bec7d50512265681cf9e32efbe4960c45353
jameslove <hello@jameslove.co.uk>
2017-09-18 15:37:58 +0100

    e2e

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { HotDocsBlogPage } from './app.po';
+
+describe('hot-docs-blog App', () => {
+  let page: HotDocsBlogPage;
+
+  beforeEach(() => {
+    page = new HotDocsBlogPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
