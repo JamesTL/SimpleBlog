23e7b5f38445b0fe6f839723667ae1145500a6ac
jameslove <hello@jameslove.co.uk>
2017-09-18 16:48:19 +0100

    updating e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('hot-docs-blog App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Blog Listing');
   });
 });
